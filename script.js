35923400c96172821d6227ff1171c925d3da781e
Janaeditz693 <vjana0640@gmail.com>
2025-08-08 11:28:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function generateImage() {
+  const prompt = document.getElementById('promptInput').value.trim();
+  if (!prompt) {
+    alert('Please enter a prompt first!');
+    return;
+  }
+  alert(`Image generation for: "${prompt}" is starting... (Functionality coming soon!)`);
+}
